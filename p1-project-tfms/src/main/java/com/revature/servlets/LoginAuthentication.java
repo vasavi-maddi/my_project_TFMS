532804a306740d7d6a7df50fc18a30ae25690632
vasavi-maddi <maddivasavi19@gmail.com>
2023-02-06 14:28:09 +0530

    p1 project

---
+++ p1-project-tfms/src/main/java/com/revature/servlets/LoginAuthentication.java
@@ -0,0 +1,50 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+//import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginAuthentication
+ */
+public class LoginAuthentication extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginAuthentication() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//PrintWriter pw= response.getWriter();
+		String u= request.getParameter("Username");
+		String p= request.getParameter("Password"); 
+		
+		if(u.equals("Vasavi") && p.equals("@123"))
+		{
+			response.sendRedirect("index2.html");
+		}
+		else {
+			response.sendRedirect("index.jsp");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
